bffb0cb8800fcaf6a0d4277d18fd11ceef572d23
shalin.lk@harness.io <ShalinLK>
2021-07-15 01:37:29 +0530

    Check now

--- main.go
+++ main.go
@@ -6,6 +6,5 @@ func main() {
 	fmt.Println("This is first line of code")
 	fmt.Println("This is second line of code")
 	fmt.Println("This is third line of code")
-	fmt.Println("we need it back")	
 
 }
