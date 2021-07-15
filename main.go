e825e0266a19d078c3f958ded1f0453cf8414bbd
shalin.lk@harness.io <ShalinLK>
2021-07-15 10:55:35 +0530

    Trigger the pipeline

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("This is first line of code")
 	fmt.Println("This is second line of code")
 	fmt.Println("This is third line of code")
+	fmt.Println("this should call it back")
 }
